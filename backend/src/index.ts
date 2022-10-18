ae7647d862740f01f035a5c3ddcafc5b961f3c63
abodesigner <mohammed.abosalim@gmail.com>
2022-10-18 18:19:08 +0200

    add middlewares

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,33 @@
 import express, { Request, Response } from "express";
+import rateLimit from 'express-rate-limit'
 const PORT = 3000;
 const app = express();
+
+app.use(express.json());
+// Apply the rate limiting middleware to all requests
+app.use(
+    rateLimit({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+        standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+        legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    })
+)
 
 app.get("/", (req: Request, res: Response) => {
     res.json({
         message : "Hello"
     })
 })
+
+app.post("/", (req: Request, res: Response) => {
+    res.json({
+        message : "Hello POST",
+        data : req.body
+    })
+})
+
+
 
 app.listen(PORT, ()=> {
     console.log(`SERVER RUNNING @PORT:${PORT}`)
